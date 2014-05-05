d9be459611406df98696d5bd12411b54a7f14f25
non-trace <yaohongxiao@126.com>
2014-05-06 01:35:22 +0800

    a

--- WebRoot/WEB-INF/sql/signupsystem.sql
+++ WebRoot/WEB-INF/sql/signupsystem.sql
@@ -4,10 +4,21 @@ Source Host: localhost
 Source Database: signupsystem
 Target Host: localhost
 Target Database: signupsystem
-Date: 2014/5/4 0:54:10
+Date: 2014/5/6 1:34:49
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+-- ----------------------------
+-- Table structure for t_class
+-- ----------------------------
+CREATE TABLE `t_class` (
+  `class_id` int(11) NOT NULL AUTO_INCREMENT,
+  `class_name` varchar(50) DEFAULT NULL,
+  `teacher` varchar(50) DEFAULT NULL,
+  `class_hour` int(50) DEFAULT NULL,
+  PRIMARY KEY (`class_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
 -- ----------------------------
 -- Table structure for t_user
 -- ----------------------------
@@ -23,9 +34,29 @@ CREATE TABLE `t_user` (
   `mobile` varchar(15) DEFAULT '' COMMENT '手机号',
   `age` int(3) DEFAULT '0' COMMENT '年龄',
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for t_user_class_rel
+-- ----------------------------
+CREATE TABLE `t_user_class_rel` (
+  `user_class_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `class_id` int(11) NOT NULL,
+  PRIMARY KEY (`user_class_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records 
 -- ----------------------------
+INSERT INTO `t_class` VALUES ('1', 'java开发', '高老师', '30');
+INSERT INTO `t_class` VALUES ('2', 'C语言开发', '王老师', '30');
+INSERT INTO `t_class` VALUES ('3', '高等数学', '李老师', '30');
+INSERT INTO `t_class` VALUES ('4', '英语', '李老师', '30');
 INSERT INTO `t_user` VALUES ('25', '啊', 'aaa', '875F26FDB1CECF20CEB4CA028263DEC6', 'man', '2014-05-14', 'a@b.com', '132201198908144579', '18611111111', '12');
+INSERT INTO `t_user` VALUES ('27', '张三', 'zhangsan', '875F26FDB1CECF20CEB4CA028263DEC6', '男', '2014-05-13', 'a@b.com', '132201198908144579', '18900898827', '2');
+INSERT INTO `t_user` VALUES ('28', '李四', 'lisi', '875F26FDB1CECF20CEB4CA028263DEC6', '男', '2014-05-15', 'a@b.com', '132201198908144579', '18900898827', '2');
+INSERT INTO `t_user_class_rel` VALUES ('1', '25', '2');
+INSERT INTO `t_user_class_rel` VALUES ('2', '25', '3');
+INSERT INTO `t_user_class_rel` VALUES ('3', '25', '1');
+INSERT INTO `t_user_class_rel` VALUES ('4', '25', '4');
